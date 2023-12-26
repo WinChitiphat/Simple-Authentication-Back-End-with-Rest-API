aba1b5fff5a2dcb56b03adcad0cb9bff5f3a8267
WinChitiphat <106735702+WinChitiphat@users.noreply.github.com>
2023-12-26 20:32:55 +0700

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Backend Server using Typescript and Rest API
+
+This project include log in authentication by having the user create, log-in, see the content in the Database and delete the user.
+
+It also introduce the creation of cookies and session token.
---
+++ src/helpers/index.ts
@@ -0,0 +1,14 @@
+import crypto from "crypto";
+
+const SECRET = "REST-API";
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (
+  salt: string | null | undefined,
+  password: string
+) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(SECRET)
+    .digest("hex");
+};
